f20d27cb76156b5e045cf683908958fe6141bc58
Ritesh Goyal <goyalritesh21@gmail.com>
2019-07-03 14:37:38 +0530

    Footer animation working

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -5,15 +5,22 @@ class Footer extends Component {
     render() {
         return (
             <Fragment>
-                <div className={"flex-center"}>
-                    <a href={"#"} className={"icon-3d"}>
-                        <i className="fab fa-twitter fa-3x"/></a>
-                    <a href={"#"} className={"icon-3d"}>
-                        <i className="fab fa-facebook fa-3x"/></a>
-                    <a href={"#"} className={"icon-3d"}>
-                        <i className="fab fa-instagram fa-3x"/></a>
-                    <a href={"#"} className={"icon-3d"}>
-                        <i className="fab fa-youtube fa-3x"/></a>
+                <div className={"flex-center social-networks bounce"}>
+                    {/*<a href={"#"} className={"icon-3d"}>*/}
+                    {/*<i className="fab fa-twitter fa-3x"/></a>*/}
+                    {/*<a href={"#"} className={"icon-3d"}>*/}
+                    {/*<i className="fab fa-facebook fa-3x"/></a>*/}
+                    {/*<a href={"#"} className={"icon-3d"}>*/}
+                    {/*<i className="fab fa-instagram fa-3x"/></a>*/}
+                    {/*<a href={"#"} className={"icon-3d"}>*/}
+                    {/*<i className="fab fa-youtube fa-3x"/></a>*/}
+                    <a href={"#"}>
+                        <i className={"fab fa-youtube fa-3x my-youtube"}/></a>
+                    <a href={"#"}>
+                        <i className="fab fa-facebook-square fa-3x my-facebook"/></a>
+                    <a href={"#"}>
+                        <i className="fab fa-instagram fa-3x my-instagram"/></a>
+
                 </div>
             </Fragment>
         );
