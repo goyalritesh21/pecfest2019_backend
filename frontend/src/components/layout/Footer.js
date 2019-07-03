b664fb3dc6ffc04648dd18928bd36f0872c8df69
Ritesh Goyal <goyalritesh21@gmail.com>
2019-07-03 15:11:59 +0530

    Final for footer with proper links

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,26 +1,16 @@
 import React, {Component, Fragment} from 'react';
-import './Footer.scss';
 
 class Footer extends Component {
     render() {
         return (
             <Fragment>
                 <div className={"flex-center social-networks bounce"}>
-                    {/*<a href={"#"} className={"icon-3d"}>*/}
-                    {/*<i className="fab fa-twitter fa-3x"/></a>*/}
-                    {/*<a href={"#"} className={"icon-3d"}>*/}
-                    {/*<i className="fab fa-facebook fa-3x"/></a>*/}
-                    {/*<a href={"#"} className={"icon-3d"}>*/}
-                    {/*<i className="fab fa-instagram fa-3x"/></a>*/}
-                    {/*<a href={"#"} className={"icon-3d"}>*/}
-                    {/*<i className="fab fa-youtube fa-3x"/></a>*/}
-                    <a href={"#"}>
+                    <a href={"https://www.youtube.com/watch?v=BWMzcA-nBQU"} target={"_blank"}>
                         <i className={"fab fa-youtube fa-3x my-youtube"}/></a>
-                    <a href={"#"}>
+                    <a href={"https://www.facebook.com/pecfestofficial/"} target={"_blank"}>
                         <i className="fab fa-facebook-square fa-3x my-facebook"/></a>
-                    <a href={"#"}>
+                    <a href={"https://www.instagram.com/pec.pecfest/"} target={"_blank"}>
                         <i className="fab fa-instagram fa-3x my-instagram"/></a>
-
                 </div>
             </Fragment>
         );
