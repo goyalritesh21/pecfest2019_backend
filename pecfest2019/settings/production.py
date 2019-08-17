74983fee389c04f28d10e693f5f1b0b014192159
unknown <madhavpruthi.becse16@pec.edu.in>
2019-08-17 15:57:23 +0530

    Modifications in production settings

--- pecfest2019/settings/production.py
+++ pecfest2019/settings/production.py
@@ -96,11 +96,7 @@ WSGI_APPLICATION = 'pecfest2019.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'PECFEST',
-        'USER': 'root',
-        'PASSWORD': 'Madhav1377',
-        'HOST': 'localhost',
-        'PORT': '',
+        'read_default_file': '/etc/mysql/my.cnf',
     }
 }
 
