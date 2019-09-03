39c1be7f8ca2e362ce8fe1ef6639f4e65a801c80
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2019-09-03 19:36:05 +0530

    Logout Fixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login', LoginAPI.as_view()),
     path('user', UserAPI.as_view()),
     path('profile', UserRegisterAPI.as_view()),
-    path('logout', knox_views.LogoutView.as_view(), name='knox_logout')
+    path('logout/', knox_views.LogoutView.as_view(), name='knox_logout')
 ]
