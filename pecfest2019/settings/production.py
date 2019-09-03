52ced871fd8611df861ed26157e3290dc1d0d9cf
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2019-09-03 14:19:31 +0530

    Production File Restored

--- pecfest2019/settings/production.py
+++ pecfest2019/settings/production.py
@@ -100,11 +100,13 @@ WSGI_APPLICATION = 'pecfest2019.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        }
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
