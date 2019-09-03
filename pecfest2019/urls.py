db26e88dc9257607ce09bc4599f4f14a179a2de2
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2019-09-03 14:00:09 +0530

    Debugging

--- pecfest2019/urls.py
+++ pecfest2019/urls.py
@@ -1,23 +1,20 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-# Routers provide an easy way of automatically determining the URL conf.
-from rest_framework import routers
 
-from accounts.views import ParticipantViewSet
-from events.views import EventViewSet, ClubViewSet, RegistrationViewSet, SponsorViewSet, UserViewSet, \
-    EventCategoryViewSet, EventTypeViewSet
 from pecfest2019 import settings
 
-router = routers.DefaultRouter()
-router.register(r'user', UserViewSet, basename='user')
-router.register(r'event', EventViewSet, basename='event')
-router.register(r'event_category', EventCategoryViewSet, basename='eventcategory')
-router.register(r'event_type', EventTypeViewSet, basename='eventtype')
-router.register(r'club', ClubViewSet, basename='club')
-router.register(r'registration', RegistrationViewSet, basename='registration')
-router.register(r'sponsor', SponsorViewSet, basename='sponsor')
-router.register(r'participant', ParticipantViewSet, basename='participant')
+# Routers provide an easy way of automatically determining the URL conf.
+#
+# router = routers.DefaultRouter()
+# router.register(r'user', UserViewSet, basename='user')
+# router.register(r'event', EventViewSet, basename='event')
+# router.register(r'event_category', EventCategoryViewSet, basename='eventcategory')
+# router.register(r'event_type', EventTypeViewSet, basename='eventtype')
+# router.register(r'club', ClubViewSet, basename='club')
+# router.register(r'registration', RegistrationViewSet, basename='registration')
+# router.register(r'sponsor', SponsorViewSet, basename='sponsor')
+# router.register(r'participant', ParticipantViewSet, basename='participant')
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
