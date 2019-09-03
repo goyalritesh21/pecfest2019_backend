b0a43261003abceecd51469e3a1085ee0084bd83
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2019-09-03 14:26:04 +0530

    DB refactoring

--- pecfest2019/urls.py
+++ pecfest2019/urls.py
@@ -11,19 +11,19 @@ from pecfest2019 import settings
 
 # Routers provide an easy way of automatically determining the URL conf.
 #
-router = routers.DefaultRouter()
-router.register(r'user', UserViewSet, basename='user')
-router.register(r'event', EventViewSet, basename='event')
-router.register(r'event_category', EventCategoryViewSet, basename='eventcategory')
-router.register(r'event_type', EventTypeViewSet, basename='eventtype')
-router.register(r'club', ClubViewSet, basename='club')
-router.register(r'registration', RegistrationViewSet, basename='registration')
-router.register(r'sponsor', SponsorViewSet, basename='sponsor')
-router.register(r'participant', ParticipantViewSet, basename='participant')
+# router = routers.DefaultRouter()
+# router.register(r'user', UserViewSet, basename='user')
+# router.register(r'event', EventViewSet, basename='event')
+# router.register(r'event_category', EventCategoryViewSet, basename='eventcategory')
+# router.register(r'event_type', EventTypeViewSet, basename='eventtype')
+# router.register(r'club', ClubViewSet, basename='club')
+# router.register(r'registration', RegistrationViewSet, basename='registration')
+# router.register(r'sponsor', SponsorViewSet, basename='sponsor')
+# router.register(r'participant', ParticipantViewSet, basename='participant')
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('api/auth/', include('accounts.urls')),
-                  url(r'^api/', include(router.urls)),
-                  url(r'^events/', include('events.urls')),
+                  # path('admin/', admin.site.urls),
+                  # path('api/auth/', include('accounts.urls')),
+                  # url(r'^api/', include(router.urls)),
+                  # url(r'^events/', include('events.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
