38f51b4a848daeaaeb7d13148a4e63cd8a98dce7
TheNsBhasin <nsbhasincool@gmail.com>
2019-09-15 00:04:24 +0530

    Fix notify_user task

--- events/tasks.py
+++ events/tasks.py
@@ -2,10 +2,13 @@ from background_task import background
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
 
+from events.models import Event
+
 
 @background(schedule=2)
-def notify_user(event, username):
+def notify_user(event_id, username):
     user = User.objects.get(username__exact=username)
+    event = Event.objects.get(id=event_id)
     email = user.email
 
     send_mail(
