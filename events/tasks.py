5372df5d546d7632de77bf17c09c6a72cc71218d
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2019-10-05 14:30:06 +0530

    modified message sending logic

--- events/tasks.py
+++ events/tasks.py
@@ -11,12 +11,17 @@ def notify_user(event_id, username):
     event = Event.objects.get(id=event_id)
     email = user.email
 
-    send_mail(
-        'Registration for ' + event.name + ' at PECFEST 2019',
-        'Thank you, ' + user.first_name.lower() + ' for registering for ' + event.name + '\nSee you there!\n'
-        'PS: Your PECFest ID is ' + username,
-        'webmasterpecfest19@gmail.com',
-        [email],
-        fail_silently=False,
-    )
+    try:
+        send_mail(
+            'Registration for ' + event.name + ' at PECFEST 2019',
+            'Thank you, ' + user.first_name.lower() + ' for registering for ' + event.name + '\nSee you there!\n'
+            'PS: Your PECFest ID is ' + username,
+            'webmasterpecfest19@gmail.com',
+            [email],
+            fail_silently=False,
+        )
+        print("mail successfully send to " + email)
+    except Exception as e:
+        print("Some error occurred while sending mail to " + email)
+
 
