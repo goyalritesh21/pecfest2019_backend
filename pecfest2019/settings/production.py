c6abe1e7213414f33965e70bea84a1b94b376bf2
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2019-10-05 19:57:42 +0530

    modified prod.py

--- pecfest2019/settings/production.py
+++ pecfest2019/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fktv0iw%mpfe$@i$nb@ln14wxiekx^=q0msiy6mztj@vr6vr98'
 # SECURITY WARNING: don't run with debug turned on in production.py!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pecfest.in', 'pecfest']
+ALLOWED_HOSTS = ['pecfest.in', 'pecfest', 'api.pecfest.in', 'admin.pecfest.in']
 
 # Application definition
 
