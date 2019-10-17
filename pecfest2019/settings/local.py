61339025efb861f069fdd7a7dfd10344c6105395
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2019-10-17 19:15:23 +0530

    production file changed

--- pecfest2019/settings/local.py
+++ pecfest2019/settings/local.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'fktv0iw%mpfe$@i$nb@ln14wxiekx^=q0msiy6mztj@vr6vr98'
 # SECURITY WARNING: don't run with debug turned on in production.py!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
--- pecfest2019/settings/production.py
+++ pecfest2019/settings/production.py
@@ -29,7 +29,7 @@ load_dotenv(dotenv_path)
 SECRET_KEY = 'fktv0iw%mpfe$@i$nb@ln14wxiekx^=q0msiy6mztj@vr6vr98'
 
 # SECURITY WARNING: don't run with debug turned on in production.py!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['pecfest.in', 'pecfest', 'api.pecfest.in', 'admin.pecfest.in']
 
