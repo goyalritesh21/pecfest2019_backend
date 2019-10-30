7ee2abde474c62365ac1f9be8961f07045416ec4
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2019-10-30 20:48:21 +0530

    added registration analytics

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,11 @@
 import datetime
+import json
 
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Count
+from django.db.models.functions import TruncDay
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from import_export.fields import Field
@@ -47,7 +52,7 @@ class RegistrationResource(resources.ModelResource):
         )
 
 
-class RegistrationAdmin(ImportExportModelAdmin):
+class RegistrationAdmin(ImportExportModelAdmin, ModelAdmin):
     resource_class = RegistrationResource
     list_filter = (
         'registered_event__association',
@@ -55,6 +60,25 @@ class RegistrationAdmin(ImportExportModelAdmin):
         'registered_event__eventType',
         'registered_event__name',
     )
+
+    change_list_template = 'admin/events/registration_list_change.html'
+
+    def changelist_view(self, request, extra_context=None):
+        # Aggregate new subscribers per day
+        chart_data = (
+            Registration.objects.annotate(date=TruncDay("record_created"))
+                .values("date")
+                .annotate(y=Count("id"))
+                .order_by("-date")
+        )
+
+        # Serialize and attach the chart data to the template context
+        as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+        extra_context = extra_context or {"chart_data": as_json}
+
+        # Call the superclass changelist_view to render the page
+        return super().changelist_view(request, extra_context=extra_context)
+
 
 
 class DetailWinnerAdmin(SimpleHistoryAdmin):
